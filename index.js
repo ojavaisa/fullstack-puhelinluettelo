960778546d1bde81dba7c75ca12e90b538021c41
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-14 16:35:50 +0200

    Tehtävä 3.3.

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get('/api/persons', (req, res) => {
     res.json(people);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = people.find(person => person.id === id);
+
+    if(person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
