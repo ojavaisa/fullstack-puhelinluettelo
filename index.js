5ae6e20b0c585f8703c74d9f6e421d7de36cffd7
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-14 17:20:32 +0200

    Tehtävä 3.6.

--- index.js
+++ index.js
@@ -63,21 +63,25 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
     const body = req.body;
 
-    if(!body.name || !body.number) {
+    if (!body.name || !body.number) {
         return res.status(400).json({
             error: 'info missing'
-        })
+        });
+    } else if (people.find(person => person.name === body.name)) {
+        //if find returns a person, it already exists and is "truthy", otherwise undefined i.e. false
+        return res.status(406).json({
+            error: 'name must be unique'
+        });
     }
-    
+
     //const id = Math.floor(Math.random() * 9999999);
     const person = {
         name: body.name,
         number: body.number,
         id: Math.floor(Math.random() * 9999999)
-    }
-    
-    console.log(person);
+    };
 
+    //console.log(person);
     people = people.concat(person);
 
     res.json(person);
