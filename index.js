e90fbc8d586faa36287f6c751009e9511032231d
Olli Väisänen <olli.vaisanen@iki.fi>
2020-12-16 16:52:49 +0200

    Frontendin buildin vienti Herokuun

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('content', (req, res) => { //custom token to log request body
     return JSON.stringify(req.body);
